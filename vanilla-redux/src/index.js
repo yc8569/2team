7bd3f8b1b91154085267616fb905418061e44b45
hyexjun <hyebyjun@gmail.com>
2022-08-02 18:25:30 +0900

    실제 DOM 요소로 추가하기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -11,7 +11,7 @@ const reducer = (state = [], action) => {
   // console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text }];
+      return [...state, { text: action.text, id: Date.now() }];
     // 배열에 push하는 등 직접 건들지 않는다. 새로운 배열 리턴
     case DELETE_TODO:
       return [];
@@ -24,6 +24,17 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 // 구독.. 왤케 와닿지를 않냐ㅠㅠ
+
+store.subscribe(() => {
+  const toDos = store.getState();
+  ul.innerHTML = ''; // 이 유무의 차이점은?
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  });
+});
 
 const onSubmit = (e) => {
   e.preventDefault();
