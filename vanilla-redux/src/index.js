88a0160508fe05c74f121c608f11502b6f5f0e7a
hyexjun <hyebyjun@gmail.com>
2022-08-02 12:48:46 +0900

    createStore 써보기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,25 +1,17 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+// const countModifier = () => {
+//   return 'hello';
+// };
 
-// number.innerText = count;
-// 이 상태로는 한 번만 innerText 되고 끝이라 함수 만들어야 됨
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => { // state = 0으로 default
+  // console.log(state);
+  return state;
 };
 
-
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+const countStore = createStore(countModifier); // reducer 함수 필요!
+console.log(countStore.getState());
